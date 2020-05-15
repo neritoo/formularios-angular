8c5baf3f4eb29f5999a439345ff4f0ae0860677f
Eze Gavilán <eze_gavilan@hotmail.com>
2020-05-15 20:27:50 -0300

    agregado: validators/campo direccion.

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.crearFormulario();
+  }
 
   ngOnInit() {
+  }
+
+  crearFormulario() {
+    this.form = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(4)]],
+      apellido: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      direccion: this.fb.group({
+        distrito: ['', Validators.required],
+        ciudad: ['', Validators.required]
+      })
+    });
+  }
+
+  get nombreNoValido() {
+    return this.form.get('nombre').invalid && this.form.get('nombre').touched;
+  }
+
+  get apellidoNoValido() {
+    return this.form.get('apellido').invalid && this.form.get('apellido').touched;
+  }
+
+  get emailNoValido() {
+    return this.form.get('email').invalid && this.form.get('email').touched;
+  }
+
+  guardar() {
+    console.log(this.form);
+
+    if(this.form.invalid) {
+      return Object.values(this.form.controls).forEach(control => {
+        control.markAsTouched();
+      });
+    }
   }
 
 }
